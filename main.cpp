4bdb16296247b3936ac191a6caeaf8dac19a46d2
Rollin Gilman <rdgilman@my.canyons.edu>
2026-03-14 19:56:54 -0700

    Completed assignment 34

--- main.cpp
+++ main.cpp
@@ -1,16 +1,26 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
+
 int main()
 {
-  string month1, month2, month3;
-  double rainfall1, rainfall2, rainfall3;
-  double avg;
+    string month1, month2, month3;
+    double rainfall1, rainfall2, rainfall3, avg;
 
-  // TODO: Read three months and their rainfall values from standard input
+    // TODO: Read three months and rainfall values
+    cin >> month1 >> rainfall1;
+    cin >> month2 >> rainfall2;
+    cin >> month3 >> rainfall3;
 
-  // TODO: Calculate the average rainfall
+    // TODO: Calculate average
+    avg = (rainfall1 + rainfall2 + rainfall3) / 3.0;
 
-  cout << fixed << setprecision(2);
-  // TODO: Print the result in the required format
+    cout << fixed << setprecision(2);
+
+    // TODO: Print result
+    cout << "The average rainfall for "
+         << month1 << ", "
+         << month2 << ", and "
+         << month3 << " is "
+         << avg << endl;
 }
